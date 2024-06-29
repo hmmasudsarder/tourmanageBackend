3956daa193d8d5b1d8b86232b9b977bcabe4ac92
hmmasudsarder <hmmasudsardar@gmail.com>
2024-06-29 17:44:35 +0600

    added user interface

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,6 +1,7 @@
+import { IUser } from "./user.interface"
 import User from "./user.model"
 
-const createUser = async(payload: string)=> {
+const createUser = async(payload: IUser)=> {
     const result = await User.create(payload)
 }
 
