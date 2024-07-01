5cef0765e95f3e8f6ea90476f45cea3ce812c822
hmmasudsarder <hmmasudsardar@gmail.com>
2024-07-01 16:50:19 +0600

    added user router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express'
+import userRouter from './module/user/user.router';
 
 const app = express();
+app.use(express.json())
 
-app.get("/", (req: Request, res: Response) =>{
-res.send("server is ongoing")
+app.use('/api/user', userRouter)
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("server is ongoing")
 })
 
 export default app;
--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from "express";
 import User from "./user.model";
 
-const createUser = async (req: Request, res: Response)=>{
-    const payload = req.body;
-    const result = await User.create(payload)
-    res.json({
-        message: "Create a user successfully",
-        data: result
-    })
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const payload = req.body;
+        const result = await User.create(payload)
+        res.json({
+            message: "Create a user successfully",
+            data: result
+        })
+    } catch (error) {
+        res.json({
+            state: false,
+            message: "Somthing went worring",
+            error
+        })
+    }
+
+
 }
 
 export const userController = {
